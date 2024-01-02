e5e045da212c0da6cb033c0f5b97d23faeb7343a
Juan Martin Silva <juanmartinsilva74@gmail.com>
2024-01-02 17:39:29 -0300

    md implement bloc

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -20,8 +20,8 @@ class HomeScreen extends StatelessWidget {
                     ),
 
                     ListTile(
-                        title: const Text('Bloc'),
-                        subtitle: const Text('Gestor de estado'),
+                        title: const Text('BloC'),
+                        subtitle: const Text('Gestor de estado compuesto'),
                         trailing: const Icon( Icons.arrow_forward_ios_rounded ),
                         onTap: () => context.push('/bloc'),
                     ),
