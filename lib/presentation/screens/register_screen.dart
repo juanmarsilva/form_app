97f05d9489dd9cc4b048144153e52f5ab3103060
Juan Martin Silva <juanmartinsilva74@gmail.com>
2024-01-06 20:47:53 -0300

    md create validate methods

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -42,25 +42,76 @@ class _RegisterView extends StatelessWidget {
     }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
     const _RegisterForm();
+
+    @override
+    State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+    String username = '';
+    String email = '';
+    String password = '';
+
+    String? Function(String?)? validateUsername = (value) {
+        if( value == null  || value.isEmpty ) return 'Campo requerido';
+
+        if( value.trim().isEmpty ) return 'Campo requerido';
+
+        if( value.length < 6 ) return 'Más de 6 letras';
+
+        return null;
+    };
+
+    String? Function(String?)? validateEmail = (value) {
+        if( value == null  || value.isEmpty ) return 'Campo requerido';
+
+        if( value.trim().isEmpty ) return 'Campo requerido';
+
+        final emailRegExp = RegExp(
+            r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+        );
+
+        if( !emailRegExp.hasMatch(value) ) return 'Correo electrónico no valido';
+
+        return null;
+    };
+
+      String? Function(String?)? validatePassword = (value) {
+        if( value == null  || value.isEmpty ) return 'Campo requerido';
+
+        if( value.trim().isEmpty ) return 'Campo requerido';
+
+        if( value.length < 6 ) return 'Más de 6 letras';
+
+        return null;
+    };
 
     @override
     Widget build(BuildContext context) {
         return Form(
+            key: _formKey,
             child: Column(
                 children: [
 
                     CustomTextFormField(
                         label: 'Nombre de usuario',
                         icon: Icons.person,
+                        onChanged: (value) => username = value,
+                        validator: validateUsername
                     ),
 
                     const SizedBox( height:  20 ),
 
                     CustomTextFormField(
                         label: 'Correo electrónico',
-                        icon: Icons.mail
+                        icon: Icons.mail,
+                        onChanged: (value) => email = value,
+                        validator: validateEmail,
                     ),
 
                     const SizedBox( height:  20 ),
@@ -69,12 +120,17 @@ class _RegisterForm extends StatelessWidget {
                         label: 'Contraseña',
                         icon: Icons.lock,
                         obscureText: true,
+                        onChanged: (value) => password = value,
+                        validator: validatePassword,
                     ),
 
                     const SizedBox( height:  20 ),
 
                     FilledButton.tonalIcon(
-                        onPressed: () {}, 
+                        onPressed: () {
+                            final isValid = _formKey.currentState!.validate();
+                            if( !isValid ) return;
+                        }, 
                         icon: const Icon( Icons.save ),
                         label: const Text('Crear usuario'),
                     )
